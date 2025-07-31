b4d6117ca1bdbeda955eeaf79df1f2e20c74be4e
ravneetk25 <ravneetkaur0725@gmail.com>
2025-07-31 12:55:10 +0530

    change

--- ll.cpp
+++ ll.cpp
@@ -7,11 +7,26 @@ struct Node{
         data= data1;
         next=next1;
     }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
 };
+Node* convert(vector<int> &arr){
+    Node* head= new Node(arr[0]);
+    Node* mover= head;
+    for(int i=0; i<arr.size(); i++){
+        Node* temp=new Node(arr[i]);
+        mover->next= temp;
+        mover= temp;
+    }
+    return head;
+}
 int main()
 {
     vector<int> arr={2, 4, 5, 8};
-    Node* y= new Node(arr[0], nullptr);
-    cout<<y;
+    //Node* y= new Node(arr[0], nullptr);
+    Node* head= convert(arr);
+    cout<<head->data;
 return 0;
 }
